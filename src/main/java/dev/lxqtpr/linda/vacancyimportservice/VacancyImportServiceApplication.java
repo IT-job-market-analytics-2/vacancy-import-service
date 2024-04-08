81efba2eabb12a744a298a91efc471f341aa13b7
lxqtpr <lxqtpr@gmail.com>
2024-04-08 18:30:43 +0300

    init project

---
+++ src/main/java/dev/lxqtpr/linda/vacancyimportservice/VacancyImportServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.lxqtpr.linda.vacancyimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyImportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyImportServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lxqtpr/linda/vacancyimportservice/VacancyImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lxqtpr.linda.vacancyimportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyImportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
